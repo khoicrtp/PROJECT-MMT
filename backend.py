8b6a19aecf94cf4ee096584bd063a641687a56fe
khoicrtp <60875449+khoicrtp@users.noreply.github.com>
2021-03-27 19:43:06 +0700

    Beta 0.1

--- backend.py
+++ backend.py
@@ -1,25 +1,9 @@
+import tkinter.messagebox
+from tkinter import *
 from array import *
 import tkinter
 from functools import partial
 import os
-
-
-file = open('weather.txt')
-Lines = file.readlines()
-# print all Lines
-for i in Lines:
-    print(i.strip())
-# read 2D-array from Lines
-a = []
-tmp = ""
-for i in range(len(Lines)):
-    tmp = Lines[i]
-    split = tmp.split()
-    a.append([(j) for j in split])
-print(a)
-print('\n')
-
-# print one row
 
 
 def printIndex(a, n):
@@ -32,29 +16,29 @@ def printIndex(a, n):
 # print all row
 
 
-def printAll(a):
-    for i in range(len(a)):
-        printIndex(a, i)
+def printAll():
+    file = open('weather.txt')
 
-
-printAll(a)
-print('\n')
-
-
-def printFind(a, str):
+    Lines = file.readlines()
+# read 2D-array from Lines
+    a = []
+    tmp = ""
+    for i in range(len(Lines)):
+        tmp = Lines[i]
+        split = tmp.split()
+        a.append([(j) for j in split])
+    result = ""
     for i in range(len(a)):
         for j in range(len(a[i])):
-            if (a[i][j] == str):
-                printIndex(a, i)
-    print('\n')
+            result += a[i][j] + " "
+        result += "\n"
 
-
-printFind(a, 'CamRanh')
+    tkinter.messagebox.showinfo("WEATHER INFORMATION", result)
 
 
 ########################################
 def getLogin():
-    loginFile = open('login.txt')
+    loginFile = open('user.txt')
     Lines = loginFile.readlines()
 
     aUsers = []
@@ -67,8 +51,6 @@ def getLogin():
 
 # ADMIN UI
 
-# ADMIN OPTION
-
 
 def updateUser():
     os.system("user.txt")
@@ -79,9 +61,8 @@ def updateData():
 
 
 def adminUI():
-    tkWindow.quit()
     ui = tkinter.Tk()
-    ui.geometry("400x150")
+    ui.geometry("600x300")
     ui.title("ADMINISTRATOR")
 
     updateDataButton = tkinter.Button(
@@ -89,71 +70,213 @@ def adminUI():
     changeUserButton = tkinter.Button(
         ui, text="Update user's info", command=updateUser).grid(row=0, column=1)
 
+    def combinedLog():
+        ui.destroy()
+        mainUI()
+    logoutButton = tkinter.Button(
+        ui, text="Logout", command=combinedLog).grid(row=2, column=7)
     ui.mainloop()
+
+# USER UI
+
+
+def printFind(find):
+    file = open('weather.txt')
+
+    Lines = file.readlines()
+# read 2D-array from Lines
+    a = []
+    tmp = ""
+    result = ""
+    for i in range(len(Lines)):
+        tmp = Lines[i]
+        split = tmp.split()
+        a.append([(j) for j in split])
+    for i in range(len(a)):
+        for j in range(len(a[i])):
+            if (a[i][j] == find.get()):
+                for k in range(len(a[i])):
+                    result += a[i][k] + " "
+                result += '\n'
+
+    tkinter.messagebox.showinfo("RESULT", result)
+
+
+def writeToFile(a, str):
+    fout = open(str)
+
+    for i in a:
+        fout.write(i)
+
+
+def changeUserPass(username, password):
+    aUsers = getLogin()
+    for i in aUsers:
+        if(i[0] == 'username'):
+            i[1] = password
+    writeToFile(aUsers, "login.txt")
+
+
+def userUI():
+    ui = tkinter.Tk()
+    ui.geometry("600x300")
+    ui.title("USER")
+
+    listAllButton = tkinter.Button(
+        ui, text="All weather data", command=printAll).grid(row=0, column=0)
+    # findDataButton = tkinter.Button(
+    #    ui, text="Find", command=findUI).grid(row=0, column=1)
+    findLabel = tkinter.Label(
+        ui, text="City, date, weather,...").grid(row=1, column=0)
+    find = tkinter.StringVar()
+
+    findEntry = tkinter.Entry(
+        ui, textvariable=find).grid(row=1, column=3)
+
+    validateFind = partial(printFind, find)
+
+    findButton = tkinter.Button(
+        ui, text="Find", command=validateFind).grid(row=1, column=7)
+
+    def combinedLog():
+        ui.destroy()
+        mainUI()
+    logoutButton = tkinter.Button(
+        ui, text="Logout", command=combinedLog).grid(row=2, column=7)
+
+    ui.mainloop()
+
 
 # MAIN UI
 
 
-def register(username, password):
+def register(Rusername, Rpassword):
     info = open("user.txt", "r")
     Lines = info.readlines()
 
-    Lines.append("\n" + username + " " + password)
+    print(Rusername.get())
+
+    Lines.append("\n" + Rusername.get() + " " + Rpassword.get())
+
+    fout = open("user.txt", "w")
+    for i in range(len(Lines)):
+        fout.write(Lines[i])
+    tkinter.messagebox.showinfo("STATUS", "Registration completed!")
+
+
+def register2(Rusername, Rpassword):
+    info = open("user.txt", "r")
+    Lines = info.readlines()
+
+    print(Rusername)
+
+    Lines.append(Rusername + " " + Rpassword)
 
     fout = open("user.txt", "w")
     for i in Lines:
         fout.write(i)
+    tkinter.messagebox.showinfo("STATUS", "Registration completed!")
 
 
-def Login(username, password):
-    if((username.get()) == "a" and (password.get()) == "a"):
-        print("Hi admin!")
-        # CHẠY UI ADMIN
-        tkWindow.destroy()
-        adminUI()
-        return
-    else:
-        aUsers = getLogin()
-        for i in range(len(aUsers)):
-            if(aUsers[i][0] == username.get() and aUsers[i][1] == password.get()):
-                print("Hi "+username.get())
-                tkWindow.destroy()
-                # CHẠY UI USER
-                return
-            elif(aUsers[i][0] == username.get()):
-                print("The account has been used. Please create a new one.")
-        print("Can't find the inputted user")
-    return
+def registerUI():
+    reg = tkinter.Tk()
+    reg.geometry('400x150')
 
-
-# MAIN
-
-register("newuws", "pass")
-
-
-tkWindow = tkinter.Tk()
-tkWindow.geometry('400x150')
-tkWindow.title('LOGIN')
+    reg.title('REGISTRATION')
 
 # username label and text entry box
-usernameLabel = tkinter.Label(tkWindow, text="User Name").grid(row=0, column=0)
-username = tkinter.StringVar()
-usernameEntry = tkinter.Entry(
-    tkWindow, textvariable=username).grid(row=0, column=1)
+    RusernameLabel = tkinter.Label(reg, text="User Name").grid(row=0, column=0)
+    Rusername = tkinter.StringVar()
+    RusernameEntry = tkinter.Entry(
+        reg, textvariable=Rusername).grid(row=0, column=1)
 
 # password label and password entry box
-passwordLabel = tkinter.Label(tkWindow, text="Password").grid(row=1, column=0)
-password = tkinter.StringVar()
-passwordEntry = tkinter.Entry(
-    tkWindow, textvariable=password, show='*').grid(row=1, column=1)
+    RpasswordLabel = tkinter.Label(reg, text="Password").grid(row=1, column=0)
+    Rpassword = tkinter.StringVar()
+    RpasswordEntry = tkinter.Entry(
+        reg, textvariable=Rpassword, show='*').grid(row=1, column=1)
 
-validateLogin = partial(Login, username, password)
+    validateReg = partial(register, Rusername, Rpassword)
+
+    def combinedFunc():
+        # validateReg
+        register(Rusername, Rpassword)
+        # register2(Rusername.get(), Rpassword.get())
+        reg.destroy()
+        mainUI()
+# reg button
+    regButton = tkinter.Button(
+        reg, text="Register", command=combinedFunc).grid(row=4, column=0)
+
+    reg.mainloop()
+
+# MAIN UI
+
+
+def mainUI():
+    def Login(username, password):
+        if((username.get()) == "a" and (password.get()) == "a"):
+            tkinter.messagebox.showinfo(
+                "WELCOME", "Welcome back, admin!")
+        # CHẠY UI ADMIN
+            mainUI.destroy()
+            adminUI()
+            return
+        else:
+            aUsers = getLogin()
+            i = 0
+            for i in range(len(aUsers)):
+                if(aUsers[i][0] == username.get() and aUsers[i][1] == password.get()):
+                    tkinter.messagebox.showinfo(
+                        "WELCOME", "Welcome back "+username.get())
+                    mainUI.destroy()
+                # CHẠY UI USER
+                    userUI()
+                    return
+                elif(aUsers[i][0] == username.get()):
+                    tkinter.messagebox.showinfo(
+                        "ERROR", "Wrong password! Please try again")
+                if(i == len(aUsers)-1):
+                    tkinter.messagebox.showinfo(
+                        "ERROR", "Invalid Login info, please create a new one")
+                    mainUI.destroy()
+                    registerUI()
+                    return
+
+    mainUI = tkinter.Tk()
+    mainUI.geometry('400x150')
+    mainUI.title('LOGIN')
+
+# username label and text entry box
+    usernameLabel = tkinter.Label(
+        mainUI, text="User Name").grid(row=0, column=0)
+    username = tkinter.StringVar()
+    usernameEntry = tkinter.Entry(
+        mainUI, textvariable=username).grid(row=0, column=1)
+
+# password label and password entry box
+    passwordLabel = tkinter.Label(
+        mainUI, text="Password").grid(row=1, column=0)
+    password = tkinter.StringVar()
+    passwordEntry = tkinter.Entry(
+        mainUI, textvariable=password, show='*').grid(row=1, column=1)
+
+    validateLogin = partial(Login, username, password)
 
 # login button
-loginButton = tkinter.Button(
-    tkWindow, text="Login", command=validateLogin).grid(row=4, column=0)
+    loginButton = tkinter.Button(
+        mainUI, text="Login", command=validateLogin).grid(row=4, column=0)
 
-tkWindow.mainloop()
+    def combinedFunc():
+        mainUI.destroy()
+        registerUI()
+
+    regButton = tkinter.Button(
+        mainUI, text="Register", command=combinedFunc).grid(row=4, column=2)
+
+    mainUI.mainloop()
 
 
 # main()
+
+mainUI()
--- database.py
+++ database.py
@@ -1,3 +1,27 @@
-import os
+# import everything from tkinter module
+from tkinter import *
 
-os.system("myfile.txt")
+# import messagebox from tkinter module
+import tkinter.messagebox
+
+# create a tkinter root window
+root = tkinter.Tk()
+
+# root window title and dimension
+root.title("When you press a button the message will pop up")
+root.geometry('500x300')
+
+# Create a messagebox showinfo
+result = ""
+
+
+def onClick():
+    tkinter.messagebox.showinfo("SEARCH RESULT",  result)
+
+
+# Create a Button
+button = Button(root, text="Click Me", command=onClick, height=5, width=10)
+
+# Set the position of button on the top of window.
+button.pack(side='bottom')
+root.mainloop()
