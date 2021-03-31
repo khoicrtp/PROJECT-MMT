d0e8b7da9a9ca47b0b90a14d1c8da5ef57780493
khoicrtp <60875449+khoicrtp@users.noreply.github.com>
2021-03-31 23:36:57 +0700

    DEMO

---
+++ DEMO/03c.bat
@@ -0,0 +1,1 @@
+python 03_client.py
--- backend.py
+++ backend.py
@@ -4,6 +4,7 @@ from array import *
 import tkinter
 from functools import partial
 import os
+import datetime
 
 
 def printIndex(a, n):
@@ -64,17 +65,18 @@ def adminUI():
     ui = tkinter.Tk()
     ui.geometry("600x300")
     ui.title("ADMINISTRATOR")
+    ui.configure(bg='light blue')
 
     updateDataButton = tkinter.Button(
-        ui, text="Update weather data", command=updateData).grid(row=0, column=0)
+        ui, text="Update weather data", bg='yellow', command=updateData).grid(row=0, column=0)
     changeUserButton = tkinter.Button(
-        ui, text="Update user's info", command=updateUser).grid(row=0, column=1)
+        ui, text="Update user's info", bg='light green', command=updateUser).grid(row=0, column=1)
 
     def combinedLog():
         ui.destroy()
         mainUI()
     logoutButton = tkinter.Button(
-        ui, text="Logout", command=combinedLog).grid(row=2, column=7)
+        ui, text="Logout", bg='orange', command=combinedLog).grid(row=0, column=10)
     ui.mainloop()
 
 # USER UI
@@ -121,9 +123,10 @@ def userUI():
     ui = tkinter.Tk()
     ui.geometry("600x300")
     ui.title("USER")
+    ui.configure(bg='light blue')
 
     listAllButton = tkinter.Button(
-        ui, text="All weather data", command=printAll).grid(row=0, column=0)
+        ui, text="All weather data", bg="light green", command=printAll).grid(row=0, column=0)
     # findDataButton = tkinter.Button(
     #    ui, text="Find", command=findUI).grid(row=0, column=1)
     findLabel = tkinter.Label(
@@ -136,13 +139,15 @@ def userUI():
     validateFind = partial(printFind, find)
 
     findButton = tkinter.Button(
-        ui, text="Find", command=validateFind).grid(row=1, column=7)
+        ui, text="Find", bg="yellow", command=validateFind).grid(row=1, column=7)
 
     def combinedLog():
+        tkinter.messagebox.showinfo(
+            "Goodbye", "Thank you for using my team's app!")
         ui.destroy()
         mainUI()
     logoutButton = tkinter.Button(
-        ui, text="Logout", command=combinedLog).grid(row=2, column=7)
+        ui, text="Logout", bg='orange', command=combinedLog).grid(row=2, column=7)
 
     ui.mainloop()
 
@@ -170,7 +175,7 @@ def register(Rusername, Rpassword):
     if checkValid(a, Rusername) == 0:
         tkinter.messagebox.showinfo(
             "ERROR", "The username is registered. Please try with another username.")
-        return
+        return 0
 
     Lines.append("\n" + Rusername.get() + " " + Rpassword.get())
 
@@ -178,30 +183,17 @@ def register(Rusername, Rpassword):
     for i in range(len(Lines)):
         fout.write(Lines[i])
     tkinter.messagebox.showinfo("STATUS", "Registration completed!")
-
-
-def register2(Rusername, Rpassword):
-    info = open("user.txt", "r")
-    Lines = info.readlines()
-
-    print(Rusername)
-
-    Lines.append(Rusername + " " + Rpassword)
-
-    fout = open("user.txt", "w")
-    for i in Lines:
-        fout.write(i)
-    tkinter.messagebox.showinfo("STATUS", "Registration completed!")
+    return 1
 
 
 def registerUI():
     reg = tkinter.Tk()
-    reg.geometry('400x150')
-
+    reg.geometry('600x300')
     reg.title('REGISTRATION')
+    reg.configure(bg='light blue')
 
 # username label and text entry box
-    RusernameLabel = tkinter.Label(reg, text="User Name").grid(row=0, column=0)
+    RusernameLabel = tkinter.Label(reg, text="Username").grid(row=0, column=0)
     Rusername = tkinter.StringVar()
     RusernameEntry = tkinter.Entry(
         reg, textvariable=Rusername).grid(row=0, column=1)
@@ -215,11 +207,14 @@ def registerUI():
     validateReg = partial(register, Rusername, Rpassword)
 
     def combinedFunc():
-        # validateReg
-        register(Rusername, Rpassword)
-        # register2(Rusername.get(), Rpassword.get())
-        reg.destroy()
-        mainUI()
+        if(Rusername.get() == '' or Rpassword == ''):
+            tkinter.messagebox.showinfo(
+                "ERROR", "Registration failed. Please check username and password again")
+            return
+        flag = register(Rusername, Rpassword)
+        if flag == 1:
+            reg.destroy()
+            mainUI()
 # reg button
     regButton = tkinter.Button(
         reg, text="Register", command=combinedFunc).grid(row=4, column=0)
@@ -260,35 +255,39 @@ def mainUI():
                     return
 
     mainUI = tkinter.Tk()
-    mainUI.geometry('400x150')
+    mainUI.geometry('600x300')
     mainUI.title('LOGIN')
+    mainUI.configure(bg='light blue')
+
+    welcomeLabel = tkinter.Label(
+        mainUI, text="WELCOME TO WEATHER APP").grid(row=0, column=0)
 
 # username label and text entry box
     usernameLabel = tkinter.Label(
-        mainUI, text="User Name").grid(row=0, column=0)
+        mainUI, text="Username").grid(row=1, column=0)
     username = tkinter.StringVar()
     usernameEntry = tkinter.Entry(
-        mainUI, textvariable=username).grid(row=0, column=1)
+        mainUI, textvariable=username).grid(row=1, column=1)
 
 # password label and password entry box
     passwordLabel = tkinter.Label(
-        mainUI, text="Password").grid(row=1, column=0)
+        mainUI, text="Password").grid(row=2, column=0)
     password = tkinter.StringVar()
     passwordEntry = tkinter.Entry(
-        mainUI, textvariable=password, show='*').grid(row=1, column=1)
+        mainUI, textvariable=password, show='*').grid(row=2, column=1)
 
     validateLogin = partial(Login, username, password)
 
 # login button
     loginButton = tkinter.Button(
-        mainUI, text="Login", command=validateLogin).grid(row=4, column=0)
+        mainUI, text="Login", bg="yellow", command=validateLogin).grid(row=1, column=2)
 
     def combinedFunc():
         mainUI.destroy()
         registerUI()
 
     regButton = tkinter.Button(
-        mainUI, text="Register", command=combinedFunc).grid(row=4, column=2)
+        mainUI, text="Register", bg="light green", command=combinedFunc).grid(row=2, column=2)
 
     mainUI.mainloop()
 
